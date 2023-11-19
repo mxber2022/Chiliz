91572eb131b2804e442b3dd5d58c93a2bb3e72ad
mxber2022 <mxber2022@gmail.com>
2023-11-19 06:55:46 +0300

    F1

--- frontend/src/components/Getplayers/Getplayers.js
+++ frontend/src/components/Getplayers/Getplayers.js
@@ -1,20 +1,39 @@
+// src/Getplayers.js
+import React, { useState } from 'react';
 import { abi } from "./abi";
-import { useContractRead } from 'wagmi';
+import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import './Getplayers.css';
 
 function Getplayers() {
+  const CONTRACT_ADDRESS = "0xAaa906c8C2720c50B69a5Ba54B44253Ea1001C98";
 
-    const { data } = useContractRead({
-        address: "0xAaa906c8C2720c50B69a5Ba54B44253Ea1001C98",
-        abi: abi,
-        functionName: 'getPlayers',
-        args: [],
-    })
+  const { config } = usePrepareContractWrite({
+    address: CONTRACT_ADDRESS,
+    abi: abi,
+    functionName: 'pickWinner',
+    args: [],
+  });
 
-    return(
-        <>
-        <>{data}</>
-        </>
-    )
+  const { data, isLoading, isSuccess, isError, write } = useContractWrite(config);
+
+  const [isWinnerPicked, setWinnerPicked] = useState(false);
+
+  const handleButtonClick = async () => {
+    await write?.();
+    setWinnerPicked(true);
+  };
+
+  return (
+    <div className={`get-players-container ${isWinnerPicked ? 'success' : ''}`}>
+      <button className={`pick-winner-button`} onClick={handleButtonClick}>
+        Pick Winner
+      </button>
+
+        {isLoading && <div>Check Wallet and Sign the transaction</div>}
+        {isSuccess && <div>'🎉 Winner Picked!'</div>}
+        
+    </div>
+  );
 }
 
 export default Getplayers;
